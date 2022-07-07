f8e9ad1e671fb031eb20d20d399833a7aadcd991
Dominik Mrówka <dmrowka7@gmail.com>
2022-07-07 20:56:32 +0200

    Add toggle favorite product

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -2,12 +2,27 @@
 export const getAll = ({ products }) => products;
 export const getCount = ({ products }) => products.length;
 
+/* actions */
+const createActionName = actionName => `app/products/${actionName}`;
+const TOGGLE_FAVORITE_PRODUCT = createActionName('TOGGLE_FAVORITE_PRODUCT');
+
+/* action creators */
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
+export const toggleFavoriteProduct = payload => ({
+  type: TOGGLE_FAVORITE_PRODUCT,
+  payload,
+});
 
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_FAVORITE_PRODUCT:
+      return statePart.map(product =>
+        product.id === action.payload
+          ? { ...product, isFavorite: !product.isFavorite }
+          : product
+      );
     default:
       return statePart;
   }
