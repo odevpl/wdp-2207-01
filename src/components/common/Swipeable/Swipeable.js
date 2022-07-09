015baef63457af3a1112121467d30ce7d143ee3f
Milosz B <miloszbargiel@gmail.com>
2022-07-09 12:51:43 +0200

    Added new Swipeable component to improve page switching and refactor NewFurniture (class -> functional)

---
+++ src/components/common/Swipeable/Swipeable.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Swipeable = ({ action, children, page, pagesNumber }) => {
+  const [moveStart, setMoveStart] = useState(0);
+  const [moveEnd, setMoveEnd] = useState(0);
+
+  const handleTouchStart = e => {
+    setMoveStart(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchMove = e => {
+    setMoveEnd(e.targetTouches[0].clientX);
+  };
+
+  const handleMouseStart = e => {
+    setMoveStart(e.clientX);
+  };
+
+  const handleMouseMove = e => {
+    setMoveEnd(e.clientX);
+  };
+
+  const handleMoveEnd = () => {
+    if (moveStart - moveEnd > 100 && page + 1 <= pagesNumber) {
+      action(page + 1);
+    }
+    if (moveStart - moveEnd < -100 && page - 1 >= 0) {
+      action(page - 1);
+    }
+  };
+
+  return (
+    <div
+      onTouchStart={e => handleTouchStart(e)}
+      onTouchMove={e => handleTouchMove(e)}
+      onTouchEnd={() => handleMoveEnd()}
+      onMouseDown={e => handleMouseStart(e)}
+      onMouseMove={e => handleMouseMove(e)}
+      onMouseUp={() => handleMoveEnd()}
+    >
+      {children}
+    </div>
+  );
+};
+
+Swipeable.propTypes = {
+  action: PropTypes.func,
+  children: PropTypes.node,
+  page: PropTypes.number,
+  pagesNumber: PropTypes.number,
+};
+
+export default Swipeable;
--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,71 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
+import Swipeable from '../../common/Swipeable/Swipeable';
 
-class NewFurniture extends React.Component {
-  state = {
-    activePage: 0,
-    activeCategory: 'bed',
-  };
+const NewFurniture = ({ categories, products }) => {
+  const [activePage, setActivePage] = useState(0);
+  const [activeCategory, setActiveCategory] = useState('bed');
 
-  handlePageChange(newPage) {
-    this.setState({ activePage: newPage });
+  const categoryProducts = products.filter(item => item.category === activeCategory);
+  const pagesCount = Math.ceil(categoryProducts.length / 8);
+
+  const dots = [];
+  for (let i = 0; i < pagesCount; i++) {
+    dots.push(
+      <li>
+        <a
+          onClick={() => setActivePage(i)}
+          className={i === activePage && styles.active}
+        >
+          page {i}
+        </a>
+      </li>
+    );
   }
 
-  handleCategoryChange(newCategory) {
-    this.setState({ activeCategory: newCategory });
-  }
-
-  render() {
-    const { categories, products } = this.props;
-    const { activeCategory, activePage } = this.state;
-
-    const categoryProducts = products.filter(item => item.category === activeCategory);
-    const pagesCount = Math.ceil(categoryProducts.length / 8);
-
-    const dots = [];
-    for (let i = 0; i < pagesCount; i++) {
-      dots.push(
-        <li>
-          <a
-            onClick={() => this.handlePageChange(i)}
-            className={i === activePage && styles.active}
-          >
-            page {i}
-          </a>
-        </li>
-      );
-    }
-
-    return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
-              <div className={'col-auto ' + styles.heading}>
-                <h3>New furniture</h3>
-              </div>
-              <div className={'col ' + styles.menu}>
-                <ul>
-                  {categories.map(item => (
-                    <li key={item.id}>
-                      <a
-                        className={item.id === activeCategory && styles.active}
-                        onClick={() => this.handleCategoryChange(item.id)}
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
-              </div>
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.panelBar}>
+          <div className='row no-gutters align-items-end'>
+            <div className={'col-auto ' + styles.heading}>
+              <h3>New furniture</h3>
+            </div>
+            <div className={'col ' + styles.menu}>
+              <ul>
+                {categories.map(item => (
+                  <li key={item.id}>
+                    <a
+                      className={item.id === activeCategory && styles.active}
+                      onClick={() => setActiveCategory(item.id)}
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={'col-auto ' + styles.dots}>
+              <ul>{dots}</ul>
             </div>
           </div>
+        </div>
+        <Swipeable action={setActivePage} page={activePage} pagesNumber={pagesCount}>
           <div className='row'>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
               <div key={item.id} className='col-3'>
@@ -73,11 +61,11 @@ class NewFurniture extends React.Component {
               </div>
             ))}
           </div>
-        </div>
+        </Swipeable>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 NewFurniture.propTypes = {
   children: PropTypes.node,
