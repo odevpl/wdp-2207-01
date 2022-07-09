6c0300fe0b4131d42a6ffe057e5db8f288c98f01
Milosz B <miloszbargiel@gmail.com>
2022-07-09 14:01:05 +0200

    Change the import of NewFurniture component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -4,7 +4,7 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
-import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import NewFurniture from '../../features/NewFurniture/NewFurniture';
 
 const Homepage = () => (
   <div className={styles.root}>
