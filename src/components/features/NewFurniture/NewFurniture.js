a7c8251b4262413e4e1c3310ddba4b718fdcdac1
Milosz B <miloszbargiel@gmail.com>
2022-07-10 17:44:30 +0200

    Add HTML structure for dropdown menu button

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 class NewFurniture extends React.Component {
   state = {
@@ -43,32 +45,38 @@ class NewFurniture extends React.Component {
       <div className={styles.root}>
         <div className='container'>
           <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
+            <div className='row no-gutters flex-column flex-wrap justify-space-between flex-md-row align-items-end'>
               <div className={'col-auto ' + styles.heading}>
                 <h3>New furniture</h3>
               </div>
-              <div className={'col ' + styles.menu}>
-                <ul>
-                  {categories.map(item => (
-                    <li key={item.id}>
-                      <a
-                        className={item.id === activeCategory && styles.active}
-                        onClick={() => this.handleCategoryChange(item.id)}
-                      >
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
+              <div className={'col ' + styles.dropdownLayout}>
+                <div className={'col ' + styles.menu}>
+                  <input id='categoryDropdown' type='checkbox' />
+                  <ul>
+                    {categories.map(item => (
+                      <li key={item.id}>
+                        <a
+                          className={item.id === activeCategory && styles.active}
+                          onClick={() => this.handleCategoryChange(item.id)}
+                        >
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className={'col-auto ' + styles.dots}>
+                  <ul>{dots}</ul>
+                  <label className={styles.dropdownButton} htmlFor='categoryDropdown'>
+                    <FontAwesomeIcon icon={faBars} />
+                  </label>
+                </div>
               </div>
             </div>
           </div>
           <div className='row'>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-3'>
+              <div key={item.id} className='col-12 col-sm-6 col-md-4 col-lg-3'>
                 <ProductBox {...item} />
               </div>
             ))}
