17affd9ee106842c415c54b7006ea26d93230844
Krzysztof Potocki <kpotocki89@gmail.com>
2022-07-10 13:15:02 +0200

    Add removing from compare in ComparedProductCard

--- src/components/features/ComparedProductCard/ComparedProductCard.js
+++ src/components/features/ComparedProductCard/ComparedProductCard.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
-
+import { useDispatch } from 'react-redux';
+import { toggleCompareProduct } from '../../../redux/productsRedux';
+import { removeFromCompare } from '../../../redux/comparedProductsRedux';
 import styles from './ComparedProductCard.module.scss';
 
-const ComparedProductCard = product => {
+const ComparedProductCard = props => {
+  const dispatch = useDispatch();
+
+  const removeCompare = e => {
+    e.preventDefault();
+    dispatch(toggleCompareProduct(props.id));
+    dispatch(removeFromCompare(props.id));
+  };
+
   return (
     <div className={styles.card}>
       <img
@@ -17,7 +27,7 @@ const ComparedProductCard = product => {
         alt='product'
       />
       <div className={styles.buttons}>
-        <Button variant='outline'>
+        <Button variant='outline' onClick={removeCompare}>
           <FontAwesomeIcon icon={faWindowClose} />
         </Button>
       </div>
