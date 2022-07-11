56a7bc003727e8f479dc1f14ee58a681f05b914d
Milosz B <miloszbargiel@gmail.com>
2022-07-11 23:56:00 +0200

    Include FeaturedBox in the homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,9 +6,11 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import PromotedBox from '../../features/PromotedBox/PromotedBox';
+import FeaturedBox from '../../features/FeaturedBox/FeaturedBox';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <FeaturedBox />
     <FeatureBoxes />
     <PromotedBox />
     <NewFurniture />
