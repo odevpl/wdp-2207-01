5e65c7fce377059b9cb69d363235a6321c07b267
Milosz B <miloszbargiel@gmail.com>
2022-07-17 14:36:24 +0200

    Add animation triggers for the carousel component in the CarouselButton

--- src/components/common/CarouselButton/CarouselButton.js
+++ src/components/common/CarouselButton/CarouselButton.js
@@ -2,23 +2,65 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CarouselButton.module.scss';
 
-const CarouselButton = ({ direction }) => {
+const CarouselButton = ({
+  direction,
+  action,
+  currentPage,
+  lastPageIndex,
+  parentFade,
+  handleParentFade,
+}) => {
+  const changePageUp = pageIdx => {
+    if (pageIdx < lastPageIndex) {
+      action(currentPage + 1);
+    }
+  };
+
+  const changePageDown = pageIdx => {
+    if (pageIdx > 0) {
+      action(currentPage - 1);
+    }
+  };
+
   if (direction === 'left') {
     return (
-      <div className={styles.button}>
+      <button
+        disabled={parentFade}
+        onClick={() => {
+          handleParentFade();
+          setTimeout(() => {
+            changePageDown(currentPage);
+          }, 250);
+        }}
+        className={styles.button}
+      >
         <span className={styles.arrow}>&lt;</span>
-      </div>
+      </button>
     );
   }
   return (
-    <div className={styles.button}>
+    <button
+      disabled={parentFade}
+      onClick={() => {
+        handleParentFade();
+        setTimeout(() => {
+          changePageUp(currentPage);
+        }, 250);
+      }}
+      className={styles.button}
+    >
       <span className={styles.arrow}>&gt;</span>
-    </div>
+    </button>
   );
 };
 
 CarouselButton.propTypes = {
   direction: PropTypes.oneOf(['left', 'right']).isRequired,
+  action: PropTypes.func,
+  currentPage: PropTypes.number,
+  lastPageIndex: PropTypes.number,
+  parentFade: PropTypes.bool,
+  handleParentFade: PropTypes.func,
 };
 
 export default CarouselButton;
