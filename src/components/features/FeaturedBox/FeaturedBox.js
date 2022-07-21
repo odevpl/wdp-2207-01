8b7831a99e27380bdfc94f4df7a91dc3a8a88e57
Milosz B <miloszbargiel@gmail.com>
2022-07-21 17:39:27 +0200

    Add functionality of a FeaturedBox component

--- src/components/features/FeaturedBox/FeaturedBox.js
+++ src/components/features/FeaturedBox/FeaturedBox.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import getAll from '../../../redux/productsRedux';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import styles from './FeaturedBox.module.scss';
 import Button from '../../common/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCaretSquareLeft,
-  faCaretSquareRight,
-} from '@fortawesome/free-regular-svg-icons';
+import { getAll } from '../../../redux/featuredRedux';
+import { getProductById } from '../../../redux/productsRedux';
+import clsx from 'clsx';
+import Swipeable from '../../common/Swipeable/Swipeable';
+import CarouselButton from '../../common/CarouselButton/CarouselButton';
 
 const FeaturedBox = () => {
-  const exampleProduct = useSelector(state => getAll(state.products)).slice(0, 1);
+  const promoImageIdxs = [1, 2];
+  const featuredProductIds = useSelector(state => getAll(state));
+  const [currentFeatured, setCurrentFeatured] = useState(0);
+  const currentProduct = useSelector(state =>
+    getProductById(state, featuredProductIds[currentFeatured])
+  );
+  const lastPromoPageIdx = promoImageIdxs.length - 1;
+  const featuredPages = featuredProductIds.length;
+  const [featuredFade, setFeaturedFade] = useState(false);
+  const [promoFade, setPromoFade] = useState(false);
+  const [autoplayStatus, setAutoplayStatus] = useState(true);
+  const [currentPromoImg, setCurrentPromoImg] = useState(0);
+
+  const changeSlide = useCallback(() => {
+    const lastItemIndex = featuredPages - 1;
+    if (autoplayStatus) {
+      setFeaturedFade(true);
+      setTimeout(() => {
+        setFeaturedFade(false);
+      }, 1000);
+      setTimeout(() => {
+        setCurrentFeatured(currentProduct => {
+          if (currentProduct === lastItemIndex) {
+            setCurrentFeatured(0);
+          }
+          return (currentProduct += 1);
+        });
+      }, 500);
+    }
+  }, [autoplayStatus, featuredPages]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeSlide();
+    }, 3500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [autoplayStatus, changeSlide]);
+
+  const manageAutoplay = () => {
+    setAutoplayStatus(false);
+    setTimeout(() => setAutoplayStatus(true), 10000);
+  };
+
+  const manageFeaturedFade = () => {
+    setFeaturedFade(true);
+    setTimeout(() => {
+      setFeaturedFade(false);
+    }, 1000);
+  };
+
+  const managePromoFade = () => {
+    setPromoFade(true);
+    setTimeout(() => {
+      setPromoFade(false);
+    }, 1000);
+  };
 
   return (
     <div className={styles.root}>
@@ -21,46 +78,77 @@ const FeaturedBox = () => {
             <div className={styles.heading}>
               <h5>Hot deals</h5>
               <ul className={styles.dots}>
-                <li>
-                  <a className={styles.active} href='#'></a>
-                </li>
-                <li>
-                  <a href='#'></a>
-                </li>
-                <li>
-                  <a href='#'></a>
-                </li>
+                {featuredProductIds.map(elem => (
+                  <li key={elem}>
+                    <button
+                      type='button'
+                      onClick={() => {
+                        manageAutoplay();
+                        manageFeaturedFade();
+                        setTimeout(() => {
+                          setCurrentFeatured(featuredProductIds.indexOf(elem));
+                        }, 500);
+                      }}
+                      className={clsx(
+                        currentFeatured === featuredProductIds.indexOf(elem) &&
+                          styles.active
+                      )}
+                    ></button>
+                  </li>
+                ))}
               </ul>
             </div>
-            <div className={styles.products}>
-              {exampleProduct.map((item, idx) => (
-                <ProductBox key={idx} {...item} isFeatured />
-              ))}
+            <div
+              onTouchStart={() => manageAutoplay()}
+              className={featuredFade ? styles.fade : ''}
+            >
+              <Swipeable
+                page={currentFeatured}
+                action={setCurrentFeatured}
+                pagesNumber={featuredPages}
+              >
+                <ProductBox {...currentProduct} isFeatured />
+              </Swipeable>
             </div>
           </div>
-          <div className={`col-8 ${styles.carouselLayout}`}>
-            <div className={styles.image}>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/featureBox_sofa.jpg`}
-                alt='sofa'
-              />
-            </div>
-            <div className={styles.promoBanner}>
-              <h2>
-                <span>Indoor</span> Furniture
-              </h2>
-              <h3>Save up to 50% of all furniture</h3>
-              <Button className={`btn-light rounded ${styles.bannerButton}`}>
-                Shop now
-              </Button>
-            </div>
+
+          <div className={clsx('col-8', styles.carouselLayout)}>
+            <Swipeable>
+              <div className={clsx(styles.image, promoFade && styles.fade)}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/featureBox${promoImageIdxs[currentPromoImg]}.jpg`}
+                  alt='sofa'
+                />
+              </div>
+              <div className={clsx(styles.promoBanner, promoFade && styles.fade)}>
+                <h2>
+                  <span>Indoor</span> Furniture
+                </h2>
+                <h3>Save up to 50% of all furniture</h3>
+                <Button className={`btn-light rounded ${styles.bannerButton}`}>
+                  Shop now
+                </Button>
+              </div>
+            </Swipeable>
             <div className={styles.buttons}>
-              <Button variant='small'>
-                <FontAwesomeIcon icon={faCaretSquareLeft}></FontAwesomeIcon>
-              </Button>
-              <Button variant='small'>
-                <FontAwesomeIcon icon={faCaretSquareRight}></FontAwesomeIcon>
-              </Button>
+              <CarouselButton
+                action={setCurrentPromoImg}
+                parentFade={promoFade}
+                handleParentFade={managePromoFade}
+                lastPageIndex={lastPromoPageIdx}
+                currentPage={currentPromoImg}
+                infinite
+                direction='left'
+              />
+              <CarouselButton
+                direction='right'
+                action={setCurrentPromoImg}
+                parentFade={promoFade}
+                handleParentFade={managePromoFade}
+                lastPageIndex={lastPromoPageIdx}
+                infinite
+                currentPage={currentPromoImg}
+              />
             </div>
           </div>
         </div>
